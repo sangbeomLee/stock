bb82d619b21faec4e3a37d8baaf7d6ee7ae81f57
sangbeomLee <wing951@naver.com>
2021-01-25 15:23:09 +0900

    add apiKey

---
+++ Xcode/stocks/API/APIKey.swift
@@ -0,0 +1,13 @@
+//
+//  Key.swift
+//  stocks
+//
+//  Created by 이상범 on 2021/01/21.
+//  Copyright © 2021 dk. All rights reserved.
+//
+
+import Foundation
+
+enum APIKey {
+    static let key = "YOUR API Key"
+}
